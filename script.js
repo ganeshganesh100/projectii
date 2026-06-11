f9293afa48408364cce2ed2122e007eb491c1db2
Ganesh Devkota <152066208+ganeshganesh100@users.noreply.github.com>
2026-06-11 13:33:22 +0545

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
-const title = document.getElementById("title");
-const changeBtn = document.getElementById("changeBtn");
+\const card = document.getElementById("card");
 
-changeBtn.addEventListener("click", function () {
-    title.textContent = "Text Changed Successfully!";
+document.addEventListener("mousemove", (e) => {
+
+    let x = (window.innerWidth / 2 - e.pageX) / 20;
+    let y = (window.innerHeight / 2 - e.pageY) / 20;
+
+    card.style.transform =
+        `rotateY(${x}deg) rotateX(${-y}deg)`;
+});
+
+document.getElementById("btn").addEventListener("click", () => {
+    alert("Welcome to 3D Animation Website!");
 });
