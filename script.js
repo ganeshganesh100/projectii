324bff30e37d392c612b3401df79a790b5cb0a67
Ganesh Devkota <152066208+ganeshganesh100@users.noreply.github.com>
2026-06-17 19:20:54 +0545

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,1 @@
-\const card = document.getElementById("card");
-
-document.addEventListener("mousemove", (e) => {
-
-    let x = (window.innerWidth / 2 - e.pageX) / 20;
-    let y = (window.innerHeight / 2 - e.pageY) / 20;
-
-    card.style.transform =
-        `rotateY(${x}deg) rotateX(${-y}deg)`;
-});
-
-document.getElementById("btn").addEventListener("click", () => {
-    alert("Welcome to 3D Animation Website!");
-});
+const cards = document.querySelectorAll(".movie-card");
