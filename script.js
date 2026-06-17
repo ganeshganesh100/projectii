c31cc661743ea5f976366aeaedf06755f87f1cdc
Ganesh Devkota <152066208+ganeshganesh100@users.noreply.github.com>
2026-06-17 19:34:21 +0545

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,22 @@
-const cards = document.querySelectorAll(".movie-card");
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
 
-cards.forEach(card => {
-    card.addEventListener("mouseenter", () => {
-        card.style.zIndex = "10";
-    });
+dotenv.config();
 
-                  card.addEventListener("mouseleave", () => {
-        card.style.zIndex = "1";
-    });
+connectDB();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/auth", require("./routes/authRoutes"));
+app.use("/api/videos", require("./routes/videoRoutes"));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on ${PORT}`);
 });
