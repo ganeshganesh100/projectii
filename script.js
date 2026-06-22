f7d529d62a25b20729d1c1a8d115afe8a9060858
Ganesh Devkota <152066208+ganeshganesh100@users.noreply.github.com>
2026-06-22 17:54:39 +0545

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
 
 
 
+/**************************************************************************
+ * SMART VIDEO PLATFORM - BACKEND SERVER
+ * ------------------------------------------------------------------------
+ * This file is the main entry point of the Node.js backend application.
+ * It is responsible for:
+ * 1. Loading environment variables.
+ * 2. Connecting to the database.
+ * 3. Initializing the Express application.
+ * 4. Configuring middleware.
+ * 5. Registering API routes.
+ * 6. Starting the server.
+ **************************************************************************/
+
