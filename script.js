bd0f456b55b3e1b2b6850f870ee1a41b5af9f0ca
Ganesh Devkota <152066208+ganeshganesh100@users.noreply.github.com>
2026-06-22 17:51:58 +0545

    Update script.js

--- script.js
+++ script.js
@@ -4,21 +4,7 @@
 
 dotenv.config();
 // Connect to the database (e.g., MongoDB) using the configuration function
-connectDB();
 
-const app = express();
-
-app.use(cors());
-app.use(express.json());
-
-app.use("/api/auth", require("./routes/authRoutes"));
-app.use("/api/videos", require("./routes/videoRoutes"));
-
-const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () => {
-  console.log(`Server running on ${PORT}`);
-});
 
 
 dotenv.config();
